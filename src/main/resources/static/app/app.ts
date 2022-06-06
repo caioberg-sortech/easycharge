6e3ec26af685edb6d216222ac101ef1cfde4ecf4
Caio-bergbjj <62817427+Caio-bergbjj@users.noreply.github.com>
2022-06-06 14:46:45 -0300

    simple-mask
    colocando o simple mask de lado, n esta funcionando

--- src/main/resources/static/app/app.ts
+++ src/main/resources/static/app/app.ts
@@ -3,11 +3,12 @@ import valida from "./validacao.js";
 const inputs  = document.querySelectorAll('input')
 const select: HTMLInputElement = document.querySelector('#estado')
 const form: HTMLFormElement  = document.querySelector('form')
-const SimpleMaskMoney =  require('simple-mask-money');
+//import * as _SimpleMaskMoney from 'simple-mask-money'
+//const SimpleMaskMoney = require('simple-mask-money') as _SimpleMaskMoney
 
 inputs.forEach(input =>{
     if(input.dataset.tipo === 'renda') {
-        SimpleMaskMoney.setMask(input, {
+       /* SimpleMaskMoney.setMask(input, {
             prefix: 'R$ ',
             fixed: true,
             fractionDigits: 2,
@@ -17,7 +18,7 @@ inputs.forEach(input =>{
         })
         form.addEventListener('submit', (evento) =>{
             input.value = SimpleMaskMoney.formatToNumber(input.value)
-        })
+        })*/
     }
     input.addEventListener('blur', function () {
         valida(input)
--- src/main/resources/static/dist/js/app.js
+++ src/main/resources/static/dist/js/app.js
@@ -2,20 +2,21 @@ import valida from "./validacao.js";
 const inputs = document.querySelectorAll('input');
 const select = document.querySelector('#estado');
 const form = document.querySelector('form');
-const SimpleMaskMoney = require('simple-mask-money');
+//import * as _SimpleMaskMoney from 'simple-mask-money'
+//const SimpleMaskMoney = require('simple-mask-money') as _SimpleMaskMoney
 inputs.forEach(input => {
     if (input.dataset.tipo === 'renda') {
-        SimpleMaskMoney.setMask(input, {
-            prefix: 'R$ ',
-            fixed: true,
-            fractionDigits: 2,
-            decimalSeparator: ',',
-            thousandsSeparator: '.',
-            cursor: 'end'
-        });
-        form.addEventListener('submit', (evento) => {
-            input.value = SimpleMaskMoney.formatToNumber(input.value);
-        });
+        /* SimpleMaskMoney.setMask(input, {
+             prefix: 'R$ ',
+             fixed: true,
+             fractionDigits: 2,
+             decimalSeparator: ',',
+             thousandsSeparator: '.',
+             cursor: 'end'
+         })
+         form.addEventListener('submit', (evento) =>{
+             input.value = SimpleMaskMoney.formatToNumber(input.value)
+         })*/
     }
     input.addEventListener('blur', function () {
         valida(input);
